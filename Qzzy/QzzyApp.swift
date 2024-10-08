cfd7f74024702be1940cd172ab7ea3b6e70bc1ad
lana.salai <lana.salai@vegait.rs>
2024-10-08 10:28:33 +0200

    Initial Commit

---
+++ Qzzy/QzzyApp.swift
@@ -0,0 +1,17 @@
+//
+//  QzzyApp.swift
+//  Qzzy
+//
+//  Created by Lana Salai on 8.10.24..
+//
+
+import SwiftUI
+
+@main
+struct QzzyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
